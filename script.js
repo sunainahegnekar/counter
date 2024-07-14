d3761b5dd3010378c54ad2ad11bca6e872900e28
Sunaina <sunainahegnekar@gmail.com>
2024-07-14 15:42:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const increase = document.getElementById("increase");
+const decrease = document.getElementById("decrease");
+const reset = document.getElementById("reset");
+const countLabel = document.getElementById("countLabel");
+let count = 0;
+
+increase.onclick = function(){
+    count++;
+    countLabel.textContent = count;
+}
+
+decrease.onclick = function(){
+    count--;
+    countLabel.textContent = count;
+}
+
+reset.onclick = function(){
+    count=0;
+    countLabel.textContent = count;
+}
